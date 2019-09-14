1bd61d41e23912e725837e98fd6dd3a81961d435
KRISHNAMURTHY-DXC, ARAVINDA <aravinda.krishnamurthy-dxc@hpe.com>
2019-09-14 22:28:08 +0530

    upd

--- src/main/java/org/dxc/ngoi/order/process/OrderMappingServiceClient.java
+++ src/main/java/org/dxc/ngoi/order/process/OrderMappingServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "ordermappingservice")
+@FeignClient(name = "ordermapping")
 public interface OrderMappingServiceClient {
 	
 	
--- src/main/java/org/dxc/ngoi/order/process/ValidateOrderServiceClient.java
+++ src/main/java/org/dxc/ngoi/order/process/ValidateOrderServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "validateorderservice")
+@FeignClient(name = "validateorder")
 public interface ValidateOrderServiceClient {
 	
 	
