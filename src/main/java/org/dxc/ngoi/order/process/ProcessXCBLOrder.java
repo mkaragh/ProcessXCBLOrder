e2efe538edd85c174f3c420c2783739522d114c7
KRISHNAMURTHY-DXC, ARAVINDA <aravinda.krishnamurthy-dxc@hpe.com>
2019-09-14 22:41:44 +0530

    upd

--- src/main/java/org/dxc/ngoi/order/process/ProcessXCBLOrder.java
+++ src/main/java/org/dxc/ngoi/order/process/ProcessXCBLOrder.java
@@ -1,5 +1,7 @@
 package org.dxc.ngoi.order.process;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -16,21 +18,27 @@ public class ProcessXCBLOrder {
 	@Autowired
 	TransactionDataServiceClient transactionDataServiceClient;
 	
+	private final Logger logger = LoggerFactory.getLogger(ProcessXCBLOrder.class);
+	
 	public boolean processOrder(String xcblOrder)
 	{
+		
+		
 		OrderMappingRequest orderMappingRequest = new OrderMappingRequest();
 		orderMappingRequest.setSourceFormat("XCBL");
 		orderMappingRequest.setTargetFormat("GSXML");
 		orderMappingRequest.setOrderDoc(xcblOrder);
 		OrderMappingResponse orderMappingResponse = orderMappingServiceClient.mapOrder(orderMappingRequest);
 		if(orderMappingResponse.statusCode.equals("200")) {
-		
+			
+			
 			ValidateOrderRequest validateOrderRequest = new ValidateOrderRequest();
 			validateOrderRequest.setInputDoc(orderMappingResponse.getOutputDoc());
 			
-			validateOrderServiceClient.validateOrder(validateOrderRequest);
+			ValidateOrderResponse validateOrderResponse = validateOrderServiceClient.validateOrder(validateOrderRequest);
 			
-			
+			if(validateOrderResponse.getStatusCode().equals("200"))
+			logger.info("Validate order is successful");
 		}
 			
 		
